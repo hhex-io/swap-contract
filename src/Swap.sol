175c775c16c354341017c0e5b065be707cada13a
Théo RICHARD <mail@rcode.tech>
2023-02-26 21:52:58 +0100

    feat: platform fee & EIP712 inheritance

--- src/Swap.sol
+++ src/Swap.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-// solhint-disable-next-line no-empty-blocks
-contract Swap {
+import {EIP712} from "openzeppelin-contracts/utils/cryptography/EIP712.sol";
 
+contract Swap is EIP712 {
+    uint256 public feeInUSD;
+
+    constructor(uint256 fee_) EIP712("Hand 2 Hand Exchange", "1") {
+        feeInUSD = fee_;
+    }
 }
