42d02e54453fe43115409a9f1fec0b9ca3c85282
Théo RICHARD <mail@rcode.tech>
2023-02-26 23:41:24 +0100

    feat(base): is valid swap

---
+++ src/SwapInternal.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ISwapInternal} from "./ISwapInternal.sol";
+
+contract SwapInternal is ISwapInternal {
+    function _isValidSwap(
+        Exchange memory exchange,
+        Data memory one,
+        Data memory two
+    ) internal view returns (bool) {
+        return true;
+    }
+}
