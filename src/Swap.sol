f37404895dc854e7eb7e65e4252d65492d9ff54f
Théo RICHARD <mail@rcode.tech>
2023-02-26 22:12:52 +0100

    feat: one party can sign their exchange data
    Data are encoded using abi.encode

--- src/Swap.sol
+++ src/Swap.sol
@@ -11,4 +11,14 @@ contract Swap is ISwapInternal, EIP712 {
     constructor(uint256 fee_) EIP712("Hand 2 Hand Exchange", "1") {
         feeInUSD = fee_;
     }
+
+    /**
+     * @dev One party sign their encoded exchange `Data` to start an exchange
+     *      within this contract and on this chain.
+     */
+    function signEncodedExchangeData(
+        bytes memory data
+    ) external view returns (bytes32 signature) {
+        signature = _hashTypedDataV4(keccak256(data));
+    }
 }
